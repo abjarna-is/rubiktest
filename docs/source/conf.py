ba99d8a879cd808657eea48ff254ae6ba0255b29
Atli Bjarnason <a@abjarna.com>
2024-05-12 01:51:35 +0000

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@ release = '0.1.0'
 extensions = [
     'sphinx_togglebutton',
     'sphinxcontrib.tikz',
+    'rst2pdf.pdfbuilder',
 ]
 
 # Configuration for sphinxcontrib-tikz
@@ -26,13 +27,16 @@ tikz_latex_preamble = r"""
 \usepackage[utf8]{inputenc}
 \usepackage[T1]{fontenc}
 \usepackage{pgfplots}
-\pgfplotsset{compat=1.17}
+\usepackage{pgfplotstable}
+\usepackage{multirow}
+\usepackage{booktabs}
+\pgfplotsset{compat=1.18, plot coordinates/math parser=false}
 \usepackage{amsmath, amsfonts, amssymb}
 \usepackage{xcolor}
-\usepackage{graphicx}
 \usepackage{hyperref}
 \usepackage{geometry}
-\usepgfplotslibrary{patchplots, polar}
+\usepackage{graphicx}
+\usepgfplotslibrary{patchplots, polar, fillbetween, ternary}
 \usetikzlibrary{arrows.meta, patterns, plotmarks, shapes.geometric, decorations.pathmorphing, decorations.text, graphs, positioning, calc, 3d}
 \geometry{a4paper, margin=1in}
 """
@@ -42,6 +46,12 @@ tikz_externalize = True
 tikz_latex_args = [r"-shell-escape"]
 
 
+
+pdf_documents = [('index', u'skjalid', u'title', u'Atli Bjarnason'),]
+  # index - master document
+  # rst2pdf - name of the generated pdf
+  # Sample rst2pdf doc - title of the pdf
+  # Your Name - author name in the pdf
 
 
 templates_path = ['_templates']
